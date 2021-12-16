121ba5384ff62b27a58b317ed7636d20d5709155
Mary Gordanier <mcgordanier@gmail.com>
2021-12-15 21:27:20 -0500

    feat: landing page layout and CMS content

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,19 +1,75 @@
 import * as React from 'react';
+import { graphql } from 'gatsby';
+import { v4 as uuidv4 } from 'uuid';
+import { GatsbyImage, getImage } from 'gatsby-plugin-image';
 import Layout from '../components/Layout';
 
-const IndexPage = () => {
+const IndexPage = ({ data }) => {
+  const content = data.allMdx.nodes[0].frontmatter;
+  const authorPhoto = getImage(content.author_photo);
   return (
     <Layout>
       <section>
-        <h1>Six Days in Rome by Francesca Giacco</h1>
-        <p>
-          In this decadent, deeply evocative novel, a young artist travels to
-          Rome to heal a broken heart, where she ​confronts loneliness and
-          intimacy, rage and desire.
-        </p>
+        <h1>Six Days in Rome by Francesca</h1>
+        <p>{content.book_pitch}</p>
+        <p>{content.book_description}</p>
+        <h2>{content.buy_heading}</h2>
+        {content.buy_button.map((button) => {
+          return (
+            <a key={uuidv4()} href={button.vendor_link}>
+              {button.vendor_name}
+            </a>
+          );
+        })}
+        {content.praise.map((praise) => {
+          return (
+            <figure key={uuidv4()}>
+              <blockquote>{praise.quote}</blockquote>
+              <figcaption>{praise.attribution}</figcaption>
+            </figure>
+          );
+        })}
+        <GatsbyImage image={authorPhoto} alt={content.author_photo_alt_text} />
+        <p>{content.mini_about}</p>
       </section>
     </Layout>
   );
 };
+
+export const query = graphql`
+  query LandingPage {
+    allMdx(filter: { frontmatter: { title: { eq: "Francesca Giacco" } } }) {
+      nodes {
+        frontmatter {
+          praise {
+            attribution
+            quote
+          }
+          buy_button {
+            vendor_link
+            vendor_name
+          }
+          author_photo {
+            childImageSharp {
+              gatsbyImageData
+            }
+          }
+          author_photo_alt_text
+          book_cover_alt_text
+          book_cover_image {
+            childImageSharp {
+              gatsbyImageData
+            }
+          }
+          book_description
+          book_pitch
+          buy_heading
+          mini_about
+          title
+        }
+      }
+    }
+  }
+`;
 
 export default IndexPage;
