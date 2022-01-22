922c2061dfebd7c861551d2cce4ed6b199a1b109
Mary Gordanier <mcgordanier@gmail.com>
2022-01-21 23:20:22 -0500

    feat: build about page - query, UI, and basic styling

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,17 +1,51 @@
 import * as React from 'react';
+import { graphql } from 'gatsby';
+import { GatsbyImage, getImage } from 'gatsby-plugin-image';
 import Layout from '../components/Layout';
 import MarkdownContent from '../components/MarkdownContent';
+import * as styles from './about.module.scss';
 
-const AboutPage = () => {
+const AboutPage = ({ data, pageContext }) => {
+  console.log('pageContext', pageContext);
+  const content = data.markdownRemark.frontmatter;
+  console.log(content);
+  const authorPhoto = getImage(content.author_photo);
   return (
     <Layout>
-      <section>
-        <div>
-          <h1>About</h1>
+      <section className={styles.aboutSection}>
+        <div className={styles.aboutBlock}>
+          <GatsbyImage
+            image={authorPhoto}
+            alt={content.author_photo_alt_text}
+            className={styles.image}
+          />
+          <div>
+            <h1>{content.page_heading}</h1>
+            <MarkdownContent tag="div" content={content.about} />
+          </div>
         </div>
       </section>
     </Layout>
   );
 };
+
+export const query = graphql`
+  query AboutPage {
+    markdownRemark(id: { eq: "536e95aa-84a0-5b3f-ab3d-5f90d45bb449" }) {
+      frontmatter {
+        about
+        author_photo {
+          childImageSharp {
+            gatsbyImageData
+          }
+        }
+        author_photo_alt_text
+        meta_description
+        page_heading
+        title
+      }
+    }
+  }
+`;
 
 export default AboutPage;
