340cb0b6373e436cceefea9956aeb7356a8eb05a
Mary Gordanier <mcgordanier@gmail.com>
2022-04-21 23:57:31 -0400

    feat: press page CMS and UI

---
+++ src/content/markdown/press.md
@@ -0,0 +1,8 @@
+---
+key: press-page
+title: Press | Francesca Giacco
+meta_description: Recent reviews and press coverage.
+page_heading: Press
+press-links:
+  - ''
+---
---
+++ src/pages/press.js
@@ -0,0 +1,44 @@
+import * as React from 'react';
+import { graphql } from 'gatsby';
+import Layout from '../components/Layout';
+import Metas from '../components/Metas';
+import MarkdownContent from '../components/MarkdownContent';
+import * as styles from './press.module.scss';
+
+const PressPage = ({ data }) => {
+  const content = data.markdownRemark.frontmatter;
+  return (
+    <Layout>
+      <Metas title={content.title} description={content.meta_description} />
+      <section className={styles.pressSection}>
+        <div className={styles.pressBlock}>
+          <h1 className={styles.pressHeading}>{content.page_heading}</h1>
+          {content.press_links.map((pressLink) => {
+            return (
+              <MarkdownContent
+                tag="div"
+                content={pressLink}
+                className={styles.pressLink}
+              />
+            );
+          })}
+        </div>
+      </section>
+    </Layout>
+  );
+};
+
+export const query = graphql`
+  query PressPage {
+    markdownRemark(frontmatter: { key: { eq: "press-page" } }) {
+      frontmatter {
+        meta_description
+        page_heading
+        title
+        press_links
+      }
+    }
+  }
+`;
+
+export default PressPage;
